cfc383cf965a39f11c8250cbd120032e5f1bd693
dtpleif <leif.ketil.solberg@gmail.com>
2020-01-30 13:15:03 +0100

    Update leif-ketil-solberg-js1-ma1.js

--- leif-ketil-solberg-js1-ma1.js
+++ leif-ketil-solberg-js1-ma1.js
@@ -138,3 +138,40 @@ console.log(div.innerHTML);
 
 
 //Question 10
+
+const div = document.querySelector(".results");
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function wrapHtmlTags(catArray) {
+  const catTags = [];
+
+  for (var i = 0; i < catArray.length; i++) {
+      let name = catArray [i].name;
+      let age = catArray[i].age;
+
+      if (!age) {
+        age = 'Age unknown';
+      }
+
+      catTags.push("<div><h5>" + name + "</h5>" + "<p>" + age + "</p></div>");
+  }
+
+    return catTags;
+}
+
+div.innerHTML = wrapHtmlTags(cats);
+
+console.log(div.innerHTML);
